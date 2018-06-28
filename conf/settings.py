1651fa1879f0f2372041f8453859a4784c91409f
echo <873956923@qq.com>
2018-06-28 16:35:02 +0800

    添加了setting中的植物贴图路径

--- conf/settings.py
+++ conf/settings.py
@@ -3,8 +3,21 @@
 """
 import pygame
 
+# 植物贴图的路径
+plantsImgDir = 'image/plants/'
 
 class Setting(object):
     def __init__(self):
         self.background = pygame.image.load('image/background1.jpg')
         self.seedBank = pygame.image.load('image/SeedBank.png')
+        self.peashooterImg = pygame.image.load(plantsImgDir + 'Peashooter/Peashooter.gif')
+        self.cherryBombImg = pygame.image.load(plantsImgDir + 'CherryBomb/CherryBomb.gif')
+        self.cherryBombBoomImg = pygame.image.load(plantsImgDir + 'CherryBomb/Boom.gif')
+        self.chomperImg = pygame.image.load(plantsImgDir + 'Chomper/Chomper.gif')
+        self.chomperAttackImg = pygame.image.load(plantsImgDir + 'Chomper/ChomperAttack.gif')
+        self.chomperDigestImg = pygame.image.load(plantsImgDir + 'Chomper/ChomperDigest.gif')
+        self.sunFlowerImg = pygame.image.load(plantsImgDir + 'SunFlower/SunFlower.gif')
+        self.wallNutImg = pygame.image.load(plantsImgDir + 'WallNut/WallNut.gif')
+        self.wallNutCrackeImg = pygame.image.load(plantsImgDir + 'WallNut/Wallnut_cracked1.gif')
+        self.wallNutBadlyCrackeImg = pygame.image.load(plantsImgDir + 'WallNut/Wallnut_cracked2.gif')
+        self.repeaterImg = pygame.image.load(plantsImgDir + 'Repeater/Repeater.gif')
